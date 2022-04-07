4a8f2f0c59a8bb3d2c7676e6726cbb9b16fd2b77
lorhof1 <61967687+lorhof1@users.noreply.github.com>
2022-04-07 20:09:08 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,6 +2,8 @@ REM get locale and save it
 powershell -Command "(Get-WinSystemLocale | Select-String -Pattern '-').toString().Replace('-', '_')" > vda
 REM extend all parts and make sure there aren't more than 10, then shrink the c:\ volume
 diskpart /s diskpart1.script && exit 1 || diskpart /s diskpart2.script || exit 1
+REM shrink 512MB parts as they would conflict with the lorOS ESP
+powershell -Command "ForEach ($line in $($(echo list` volume | diskpart | Select-String -Pattern '512 MB'.toString()) -split '`r`n')) { echo ('select volume ' + $line.Split('')[3] + '`nshrink desired=4') | diskpart }"
 REM install lorOS
 qemu\qemu-system-x86_64 -cdrom install.iso -drive file=\\.\PHYSICALDRIVE0,format=raw -drive file=vda,if=virtio -drive file=vdb,if=virtio -drive file=vdc,if=virtio -drive file=vdd,if=virtio -drive file=vde,if=virtio -m 2G -boot order=d -bios uefi-bios.bin -display none
 REM register the bootloader
